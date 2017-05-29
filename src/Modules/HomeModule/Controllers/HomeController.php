359b198e5baa502b0b3969ea3c082b8474ee9430
mariusz <hp@DESKTOP-6IIBR1E.local>
2017-05-29 16:19:52 +0200

    Add json file for configuration

---
+++ src/Modules/HomeModule/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Modules\HomeModule\Controllers;
+/**
+ * Description of HomeController
+ *
+ * @author hp
+ */
+class HomeController {
+    public function get(){
+        echo 1;
+    }
+}
